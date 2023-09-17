439a8dbe0014a393cf686f9b5e2f9e8139d91989
Coder_94 <101722817+Coder-94@users.noreply.github.com>
2023-09-17 23:20:21 +0900

    2023/09/17 시작화면 구현
    틀 구현 완료

---
+++ 2D_Project/Assets/Scripts/UI/Main Scene/btnType.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum BTNTYPE
+{
+    Start,
+    Help,
+    Option,
+    Sound,
+    Back,
+    Quit
+}
+
+public class btnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    
+    public BTNTYPE      currentType;
+    public Transform    buttonScale;
+    Vector3             defaultScale;
+
+    private void Start()
+    {
+        defaultScale = buttonScale.localScale;
+    }
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BTNTYPE.Start:
+                break;
+            case BTNTYPE.Help:
+                break;
+            case BTNTYPE.Option:
+                break;
+            case BTNTYPE.Sound:
+                break;
+            case BTNTYPE.Back:
+                break;
+            case BTNTYPE.Quit:
+                break;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale;
+    }
+}
