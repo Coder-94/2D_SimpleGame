8c084a9d8494c766cbfa2b447bb5c5d77dffb7bc
skyfish811 <zeoryubin@gmail.com>
2023-11-11 05:07:38 +0900

    수정했는데 안돼 살려줘

--- 2D_Project/Assets/Scripts/Player/PlayerMove.cs
+++ 2D_Project/Assets/Scripts/Player/PlayerMove.cs
@@ -6,62 +6,76 @@ public class PlayerMove : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rb;
+    public LayerMask Ground;
+    public bool isGrounded = false;
+    public RaycastHit2D hit;
+    public Vector3 Grounded;
     //Move 함수
     public float moveSpeed = 1f;
     public Vector3 BeforePo = new Vector3(0,0,0);
     //Jump 함수
-    public bool isGrounded;
-    public LayerMask Ground;
     public float jumpForce = 5f;
-    public bool isJumped;
-
+    public bool isJumped = false;
 
 
     private void OnEnable()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        isJumped = false;
     }
     private void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, Ground);
+        Grounded = hit.point;
+        hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, Ground);
         isGrounded = (hit.collider != null);
+        BeforePo = transform.position;
+        Move();
+        Crouch();
         if (isGrounded && isJumped)
         {
             animator.SetTrigger("Jump");
             isJumped = false;
         }
-        BeforePo = transform.position;
-        Move();
         Jump();
-        Crouch();
-        if (transform.position == BeforePo)
+        if (transform.position == BeforePo && isGrounded)
         {
             animator.SetInteger("walk", 0);
         }
 
     }
+    private void FixedUpdate()
+    {
+
+    }
     void Move()
     {
         Vector3 movePosisiton = Vector3.zero;
-       
+
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
             movePosisiton = Vector3.left;
             transform.localScale = new Vector3(-1, 1, 1);
-            if (!isJumped)
+            if (isGrounded)
             {
                 animator.SetInteger("walk", 1);
             }
+            else
+            {
+                animator.SetInteger("walk", 0);
+
+            }
         }
-        else if(Input.GetAxisRaw("Horizontal") > 0)
+        else if (Input.GetAxisRaw("Horizontal") > 0)
         {
             movePosisiton = Vector3.right;
             transform.localScale = new Vector3(1, 1, 1);
-            if (!isJumped)
+            if (isGrounded) { 
+            animator.SetInteger("walk", 1);
+            }
+            else
             {
-                animator.SetInteger("walk", 1);
+                animator.SetInteger("walk", 0);
+
             }
 
 
@@ -72,24 +86,29 @@ public class PlayerMove : MonoBehaviour
     }
     void Crouch()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (isGrounded)
         {
-            animator.SetTrigger("Crouch");
-        }
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            animator.SetTrigger("Crouch");
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                animator.SetTrigger("Crouch");
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                animator.SetTrigger("Crouch");
+            }
         }
     }
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            animator.SetTrigger("Jump");
-            isJumped = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                animator.SetTrigger("Jump");
+                isJumped = true;
+            }
         }
-        
     }
 
 }
