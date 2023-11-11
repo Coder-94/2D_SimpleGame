7e458972452df7052fa93cde8e7de6fb9b76a001
jh616 <101758537+jh616@users.noreply.github.com>
2023-11-11 17:42:41 +0900

    player임시파일
    player임시파일

--- 2D_Project/Assets/Scripts/Enemy/TemporaryFile.cs
+++ 2D_Project/Assets/Scripts/Enemy/TemporaryFile.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TemporaryFile : MonoBehaviour
-{ 
+{
+    Animator animator;
+
     public int maxHp;
     public int nowHp;
     public int atkDmg;
@@ -24,12 +26,33 @@ public class TemporaryFile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetInteger("walk", 0);
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetInteger("walk", 0);
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        else animator.SetInteger("walk", 0); ;
+
+        if (Input.GetKeyDown(KeyCode.A) &&
+            !animator.GetCurrentAnimatorStateInfo(0).IsName("WizardAttack"))
+        {
+            animator.SetTrigger("Attack");
+        }
+
     }
 }
